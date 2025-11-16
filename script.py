4ea3c2955f1320c05c08c818987c34bf609ec61a
Trésor Sembeya <sembeyat@gmail.com>
2025-11-16 11:27:21 +0100

    Create script.py for data visualization
    Added a script that visualizes data using matplotlib and pandas.

---
+++ script.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt  
+# … ton code de visualisation …
+print("Coucou depuis GitHub")
+import matplotlib.pyplot as plt  
+import pandas as pd  
+
+# Exemple de code
+data = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
+plt.plot(data['x'], data['y'])
+plt.show()
